7dbd6d61ad6e5959825edf1df4665b499ab89c1b
Denis Martynov <idenismartynov@yandex.ru>
2025-03-10 00:28:51 +0300

    Пишу дальше свой ИИ

--- src/main/java/org/example/Players/Explorer.java
+++ src/main/java/org/example/Players/Explorer.java
@@ -16,12 +16,24 @@ public class Explorer {
     }
 
     private void updatePlayerRelations(int playerNumber, int aggressiveDelta, int diplomacyDelta) {
-        players[playerNumber].setAggressiveScore(players[playerNumber].getAggressiveScore() + aggressiveDelta);
-        players[playerNumber].setDiplomacyScore(players[playerNumber].getDiplomacyScore() + diplomacyDelta);
+        // Изменяем значения
+        int newAggressiveScore = players[playerNumber].getAggressiveScore() + aggressiveDelta;
+        int newDiplomacyScore = players[playerNumber].getDiplomacyScore() + diplomacyDelta;
+
+        // Ограничиваем значения в диапазоне от -200 до 200
+        newAggressiveScore = Math.max(-200, Math.min(200, newAggressiveScore));
+        newDiplomacyScore = Math.max(-200, Math.min(200, newDiplomacyScore));
+
+        // Устанавливаем новые значения
+        players[playerNumber].setAggressiveScore(newAggressiveScore);
+        players[playerNumber].setDiplomacyScore(newDiplomacyScore);
+
+        // Выводим информацию об изменении отношений
         System.out.println("Отношения игрока " + (playerNumber + 1) + " изменились и теперь они составляют: ");
         System.out.println("!!!" + "Агрессивность: " + players[playerNumber].getAggressiveScore() +
                 " Дипломатия: " + players[playerNumber].getDiplomacyScore());
     }
+
 
     // Метод проверки, был ли я атакован другими игроками за ход - в итоге я развязываю с ними войну в ответ
     public void doWeAttacked() {
@@ -51,6 +63,7 @@ public class Explorer {
         }
     }
 
+
     // Метод позволяющий прийти к соглашению о мире с рандомным игроком с рандомными условиями
     public void canWeMakePeace() {
         for (int i = 0; i < players.length; i++) { // Используем индекс
@@ -63,10 +76,14 @@ public class Explorer {
                     int answer1 = scanner.nextInt();
                     if (answer1 == 1) {
                         System.out.println("=== ЗАКЛЮЧАЕМ СДЕЛКУ! ТЫ НЕ МОЖЕШЬ НА МЕНЯ НАПАДАТЬ 5 ХОДОВ! ===");
+                        System.out.println(" ");
+
                         player.setWar(false);
                         updatePlayerRelations(i, 200, 50); // Используем индекс игрока
                     } else {
                         System.out.println("=== НЕ УДАЛОСЬ ДОГОВОРИТЬСЯ! ===");
+                        System.out.println(" ");
+
                     }
                 }
             }
@@ -75,7 +92,7 @@ public class Explorer {
 
 
 
-    public void firstAction() {
+    public void firstAction() {0
         System.out.println("ВРАГИ В ПРЕДЕЛАХ 1 ЗОНЫ? (1 - Да, 0 - Нет)");
         int isEnemyNear = scanner.nextInt();
         if (isEnemyNear == 1) {
@@ -110,6 +127,8 @@ public class Explorer {
                     Random random = new Random();
                     if (random.nextInt(100) >= 65 ) {
                         System.out.println("=== СТРЕЛЯЮ БАШНЯМИ ПО ВРАГАМ И УБИВАЮ ПО 1 ЮНИТУ У САМЫХ УГРОЖАЮЩИХ АРМИЙ ЗА КАЖДУЮ БАШНЮ ===");
+                        System.out.println(" ");
+
                     } else {
                         System.out.println("промахнулся");
                     }
@@ -121,6 +140,8 @@ public class Explorer {
                     Random random = new Random();
                     if (random.nextInt(100) >= 50) {
                         System.out.println("=== ИСПОЛЬЗУЮ ЗАКЛИНАНИЕ! ===");
+                        System.out.println(" ");
+
                     } else {
                         System.out.println("промахиваюсь заклинанием");
                     }
@@ -129,9 +150,16 @@ public class Explorer {
                 }
 
                 System.out.println("=== ГОТОВЛЮСЬ К ОБОРОНЕ, НО ДВА ОТРЯДА ЗАХВАТЫВАЮТ ОБЛАСТИ ===");
+                System.out.println(" ");
+
+            } else {
+                System.out.println("=== ОБОРОНЯЮСЬ! ===");
+                System.out.println(" ");
             }
         } else if (isEnemyNear == 0) {
             System.out.println("=== ЗАНИМАЮСЬ ЗАХВАТОМ ОБЛАСТЕЙ ===");
+            System.out.println(" ");
+
         }
     }
 
@@ -140,24 +168,36 @@ public class Explorer {
 
         System.out.println("Случайное событие произошло!");
         System.out.println("КТО-ТО ХОЧЕТ ЗАХВАТИТЬ ЦЕННЫЕ РЕСУРСЫ? (1 - Да, 0 - Нет)");
+        System.out.println(" ");
+
         int answer = scanner.nextInt();
             if (answer == 1) {
                 System.out.println("У МЕНЯ МАНЫ БОЛЬШЕ ЧЕМ 6? (1 - Да, 0 - Нет)");
+                System.out.println(" ");
+
                 int answer2 = scanner.nextInt();
                 System.out.println("ВЫБЕРИТЕ ИГРОКА (Цифру от 1 до " + players.length + "):");
+                System.out.println(" ");
+
                 int playerNumber = scanner.nextInt() - 1; // Индексация с 0
                 System.out.println("Выбран игрок: " + (playerNumber + 1));
 
                 if (playerNumber >= 0 && playerNumber < players.length) {
                     if (answer2 == 1) {
                         System.out.println("!!! Я ПРЕДУПРЕЖДАЮ ТЕБЯ ОТСТУПИТЬ И НЕ ЗАХВАТЫВАТЬ!!! (1 - Да, 0 - Нет)");
+                        System.out.println(" ");
+
                         int answer3 = scanner.nextInt();
                         if (answer3 == 1) {
                             System.out.println("=== ДОГОВОРИЛИСЬ! ТЫ НЕ ЗАХВАТЫВАЕШЬ ЭТУ ЗОНУ ===");
+                            System.out.println(" ");
+
                             updatePlayerRelations(playerNumber, 30, 30); // Улучшение отношений
                         } else {
-                        System.out.println("!!! Я УДАРЯЮ ЗАКЛИНАНИЕМ ПО ТВОИМ ЮНИТАМ И УБИВАЮ:" + random.nextInt(3));
-                        updatePlayerRelations(playerNumber, -10, -25); // Ухудшение отношений
+                        System.out.println("!!! Я УДАРЯЮ ЗАКЛИНАНИЕМ ПО ТВОИМ ЮНИТАМ И УБИВАЮ:" + random.nextInt(4));
+                            System.out.println(" ");
+
+                            updatePlayerRelations(playerNumber, -10, -25); // Ухудшение отношений
                         }
                     }
                 } else {
@@ -165,33 +205,49 @@ public class Explorer {
                 }
             } else {
                 System.out.println("=== ПРОДОЛЖАЮ ЗАХВАТЫВАТЬ ТЕРРИТОРИИ ===");
+                System.out.println(" ");
+
             }
     }
 
     // Метод используется если ведется война с игроком
     public void attackTargetOnWar() {
         Random random = new Random();
-
+        int randomNumber = random.nextInt(100);
         for (int i = 0; i < players.length; i++) { // Проходим по всем игрокам
             Player player = players[i];
             if (player.isWar()) { // Проверяем, ведется ли война с этим игроком
-                if (random.nextInt(100) >= 60) {
-                    System.out.println("=== ВЫДВИГАЮ ЧАСТЬ СВОИХ ВОЙСК ПРОТИВ ИГРОКА: " + (i + 1) + " ===");
-                }
-            } else {
-                System.out.println("№1 ТАРГЕТИНГ: Я МОГУ ОПРЕДЕЛИТЬ ПРИОРИТЕТНУЮ ЦЕЛЬ НА ОСНОВЕ ЕГО СИЛЫ? (1 - Да, 0 - Нет)");
-                int answer1 = scanner.nextInt();
+                if (randomNumber >= 80) {
+                    System.out.println("=== ФОКУСИРУЮСЬ ПРОТИВ ПРОТИВ ИГРОКА: " + (i + 1) + " ===");
+                    System.out.println(" ");
 
-                if (answer1 == 1) {
-                    System.out.println("=== ДЕЙСТВУЮ ПРОТИВ СЛАБЕЙШИХ ВРАЖЕСКИХ АРМИЙ, КОТОРАЯ НАХОДИТСЯ ПОБЛИЗОСТИ ОТ МОИХ ТЕРРИТОРИЙ ===");
+                } else {
+                    System.out.println("№1 ТАРГЕТИНГ: Я МОГУ ОПРЕДЕЛИТЬ ПРИОРИТЕТНУЮ ЦЕЛЬ НА ОСНОВЕ ЕГО СИЛЫ? (1 - Да, 0 - Нет)");
+                    System.out.println(" ");
+                    int answer1 = scanner.nextInt();
+                    if (answer1 == 1) {
+                        System.out.println("=== ДЕЙСТВУЮ ПРОТИВ СЛАБЕЙШИХ ВРАЖЕСКИХ АРМИЙ, КОТОРАЯ НАХОДИТСЯ ПОБЛИЗОСТИ ОТ МОИХ ТЕРРИТОРИЙ ===");
+                        System.out.println(" ");
 
-                    System.out.println("№2 РАЗВИТИЕ НАСТУПЛЕНИЯ: МОГУ ЛИ Я НАЧАТЬ НАСТУПАТЬ НА ОДНОГО ИЗ ИГРОКА? (1 - Да, 0 - Нет)");
-                    int answer2 = scanner.nextInt();
-                    if (answer2 == 1) {
-                        System.out.println("=== ПРОДОЛЖАЮ НАСТУПЛЕНИЕ ЧАСТЬЮ СВОЕЙ АРМИИ ПРОТИВ ИГРОКА");
+                        // ВОТ ТУТ ДЛЯ ДРУГОГО ИИ ИГРОКА МОЖНО СДЕЛАТЬ ДРУГИЕ ЗНАЧЕНИЯ ЛИБО ВООБЩЕ СДЕЛАТЬ 100% ЧТО ОН ПОЙДЕТ В АТАКУ БОЛЬШИМ ЧИСЛОМ
+                        int random2 = random.nextInt(100);
+                        if (random2 > 70) {
+                            System.out.println("№2 РАЗВИТИЕ НАСТУПЛЕНИЯ: МОГУ ЛИ Я НАЧАТЬ НАСТУПАТЬ НА ОДНОГО ИЗ ИГРОКА? (1 - Да, 0 - Нет)");
+                            int answer2 = scanner.nextInt();
+                            if (answer2 == 1) {
+                                int units = random.nextInt(4);
+                                System.out.println("=== ПРОДОЛЖАЮ НАСТУПЛЕНИЕ ДО " + units + "ЮНИТАМИ ПРОТИВ ИГРОКА С КЕМ ВЕЛ СРАЖЕНИЕ ===");
+                                System.out.println(" ");
+                            }
+                        }
+                    } else {
+                        System.out.println("=== ЗАНИМАЮ ГЛУХУЮ ОБОРОНУ! ===");
+                        System.out.println(" ");
+
                     }
                 }
             }
+            break;
         }
     }
 
@@ -203,9 +259,13 @@ public class Explorer {
         System.out.println("В конце хода я покупаю " + random.nextInt(4) + " юнитов");
         if (random.nextInt(100) >= 70) {
             System.out.println("В конце хода я покупаю 1 башню!");
+            System.out.println(" ");
+
         }
         if (random.nextInt(100) >= 90) {
             System.out.println("В конце хода я покупаю 1 замок(+3)");
+            System.out.println(" ");
+
         }
     }
 
@@ -214,6 +274,8 @@ public class Explorer {
 
         System.out.println("В конце хода я хожу " + random.nextInt(4) + " отрядами нейтральных народов");
         System.out.println("Восстанавливаю потери " + random.nextInt(4) + " у нейтральных народов");
+        System.out.println(" ");
+
     }
 
 
